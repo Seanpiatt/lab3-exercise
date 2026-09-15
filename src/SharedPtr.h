5d318e7d2e70d995af9cb9f192f9e47ec92dc110
Sean Piatt <sdpiatt@cs.ualberta.ca>
2026-09-15 14:24:26 -0600

    Setup shardptr template

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -34,4 +34,79 @@ private:
     // TODO: add field(s) which both control block types need to have
 };
 
+template <typename T>
+class ShredPtr {
+public:
+    ShredPtr() : mItem(nullptr) {}
+    explicit ShredPtr(T* item) : mItem(item) {}
+    ~ShredPtr() { delete mItem; }
+
+    ShredPtr(const ShredPtr&) = delete;
+    ShredPtr& operator=(const ShredPtr&) = delete;
+
+    ShredPtr(ShredPtr&& other) noexcept : mItem(other.mItem) {
+        other.mItem = nullptr;
+    }
+
+    ShredPtr& operator=(ShredPtr&& other) noexcept {
+        if (this != &other) {
+            delete mItem;
+            mItem = other.mItem;
+            other.mItem = nullptr;
+        }
+        return *this;
+    }
+
+    T* release() {
+        T* temp = mItem;
+        mItem = nullptr;
+        return temp;
+    }
+
+    template <typename U>
+    ShredPtr(ShredPtr<U>&& other) : mItem(other.release()) {}
+
+    T& operator*() const {
+        assert(mItem != nullptr);
+        return *mItem;
+    }
+
+    T* operator->() const {
+        assert(mItem != nullptr);
+        return mItem;
+    }
+
+    T* get() const {
+        return mItem;
+    }
+
+    bool operator==(const ShredPtr& other) const {
+        return mItem == other.mItem;
+    }
+
+    void reset(T* newPtr = nullptr) {
+        T* old = mItem;
+        mItem = newPtr;
+        delete old;
+    }
+
+    void swap(ShredPtr& other) noexcept {
+        T* temp = mItem;
+        mItem = other.mItem;
+        other.mItem = temp;
+    }
+
+    explicit operator bool() const {
+        return mItem != nullptr;
+    }
+
+private:
+    T* mItem;
+};
+
+template <typename T, typename... Args>
+ShredPtr<T> makeUnique(Args&&... args) {
+    return ShredPtr<T>(new T(std::forward<Args>(args)...));
+}
+
 #endif
